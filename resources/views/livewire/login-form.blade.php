23cb934317f071d5f07931ce8994394b8fb925e8
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-02-28 10:55:55 -0300

    tela de login inicialmente finalizada

--- resources/views/livewire/login-form.blade.php
+++ resources/views/livewire/login-form.blade.php
@@ -9,17 +9,32 @@
         <div class="row mt-4">
             <div class="col-md-12">
                 <input type="text" name="email" class="my-custom-input" placeholder="seu@email.com">
+                <span class="text-danger mt-1">Error msg</span>
             </div>
         </div>
         <div class="row mt-4">
             <div class="col-md-12">
                 <input type="password" name="senha" class="my-custom-input" placeholder="******">
+                <span class="text-danger mt-1">Error msg</span>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="form-check form-switch">
+                    <input class="form-check-input pointer" type="checkbox" value="" id="lembrar_me" name="lembrar_me" style="accent-color: red;">
+                    <label class="form-check-label pointer" for="lembrar_me">
+                      Lembrar de mim
+                    </label>
+                </div>
             </div>
         </div>
         <div class="row mt-5">
             <div class="col-md-12">
                 <button type="submit" class="login-btn">
                     Entrar
+                </button>
+                <button type="button" class="login-btn-blue mt-4" title="Login facebook" style="padding-top: 10px">
+                    <i class="fa-brands fa-facebook fa-2xl"></i>
                 </button>
             </div>
         </div>

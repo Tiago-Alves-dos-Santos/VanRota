9cb6c81aefd2085bd5df81262ca43eeb2e14fae0
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-03-09 16:52:35 -0300

    navbar responsiva

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -28,7 +28,7 @@
         <div class="sublink">
             <div class="sublink-title">
                 <span>Titulo</span>
-                <i class="fa-solid fa-angle-right"></i>
+                <i class="fa-solid fa-square-caret-down"></i>
             </div>
             <div class="sublink-links">
                 <a href="">
@@ -55,7 +55,65 @@
 @push('layout.sidebar.scripts')
 <script>
     $(function(){
-
+        //variaveis globais
+        let mysidebar = $("#mysidebar-geral");
+        let container_body = $("#content-sidebar");
+        //functions-events
+        function verfiyWindow(event = true){
+            let breakpoint = 1000;
+            if($(window).width() <= breakpoint){//sidebar não visivel
+                sidebarNaoVisivel();
+            }else if($(window).width() > breakpoint && event == true){
+                sidebarVisivel();
+            }
+        }
+        verfiyWindow(false);
+        function sidebarWindowResise(){
+            $(window).resize(function() {
+                verfiyWindow();
+            });
+        }
+        sidebarWindowResise();
+        function sidebarMaxHeight(){
+            $(document).on('scroll', function(){
+                $(mysidebar).height(document.body.scrollHeight);
+            });
+        }
+        sidebarMaxHeight();
+        function sidebarVisivel(){
+            $(container_body).removeClass('container-sidebar-saida');
+            $(container_body).addClass('container-sidebar-entrada');
+            $(mysidebar).removeClass('sidebar-saida');
+            $(mysidebar).addClass('sidebar-entrada');
+            $(mysidebar).data('show','visivel');
+        }
+        function sidebarNaoVisivel(){
+            $(container_body).removeClass('container-sidebar-entrada');
+            $(container_body).addClass('container-sidebar-saida');
+            $(mysidebar).removeClass('sidebar-entrada');
+            $(mysidebar).addClass('sidebar-saida');
+            $(mysidebar).data('show','nao-visivel');
+        }
+        function toggleSidebar(){
+            $("#toggle-sidebar").on('click', function(e){
+                let visivel = $(mysidebar).data('show') == 'visivel' ? true:false;
+                if(visivel){//visivel
+                    sidebarNaoVisivel();
+                }else{
+                    sidebarVisivel();
+                }
+            });
+        }
+        toggleSidebar();
+        //esconder sublinks
+        $(".sublink-links").hide();
+        function toggleSideSubLinks(){
+            $(".sublink-title").on('click', function(){
+                let sublinks = $(this).parent().find('.sublink-links');
+                $(sublinks).slideToggle();
+            });
+        }
+        toggleSideSubLinks();
     });
 </script>
 @endpush
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -11,30 +11,9 @@
     @stack('layout.sidebar.scripts')
     <script>
         $(function(){
-            //esconder sublinks
-            // $(".sublink-links").hide();
-            function toggleSidebar(){
-                let mysidebar = $("#mysidebar-geral");
-                let container_body = $("#content-sidebar");
-                $("#toggle-sidebar").on('click', function(e){
-                    let visivel = $(mysidebar).data('show') == 'visivel' ? true:false;
-                    if(visivel){//visivel
-                        $(container_body).removeClass('container-sidebar-entrada');
-                        $(container_body).addClass('container-sidebar-saida');
-                        $(mysidebar).removeClass('sidebar-entrada');
-                        $(mysidebar).addClass('sidebar-saida');
-                        $(mysidebar).data('show','nao-visivel');
-                    }else{
-                        $(container_body).removeClass('container-sidebar-saida');
-                        $(container_body).addClass('container-sidebar-entrada');
-                        $(mysidebar).removeClass('sidebar-saida');
-                        $(mysidebar).addClass('sidebar-entrada');
-                        $(mysidebar).data('show','visivel');
-                    }
-                });
-            }
-            toggleSidebar();
+
         });
     </script>
+    <script src="{{ asset('js/footer.js') }}"></script>
 </body>
 </html>
